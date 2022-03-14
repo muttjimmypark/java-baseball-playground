0a8bdf13680b4fee8a9b3d98d3fcf7ebf75256e1
muttjimmypark <muttpark57@gmail.com>
2022-03-14 23:16:23 +0900

    studying string testcase 3-2

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,11 @@
 package study;
 
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -27,10 +30,12 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("charAt() test - assertThat ThrownBy를 활용")
     void charAt(){
         String actual = "abc";
 
         //기존 테스트 후, 위치값 벗어나는 경우 확인
+        //charAt으로 추출하니까 외따옴표
         assertThat(actual.charAt(0)).isEqualTo('a');
         assertThat(actual.charAt(1)).isEqualTo('b');
         assertThat(actual.charAt(2)).isEqualTo('c');
@@ -39,6 +44,23 @@ public class StringTest {
             char val = actual.charAt(3);
             throw new Exception("범위 초과");
         }).isInstanceOf(IndexOutOfBoundsException.class)
-                .hasMessageContaining("String");
+                .hasMessageContaining("charAt() - Index out of bounds issue");
+    }
+
+    @Test
+    @DisplayName("charAt() test - assertThat ExceptionOfType를 활용")
+    void charAt2(){
+        String actual = "abc";
+
+        assertThat(actual.charAt(0)).isEqualTo('a');
+        assertThat(actual.charAt(1)).isEqualTo('b');
+        assertThat(actual.charAt(2)).isEqualTo('c');
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy( () -> {
+                    char val = actual.charAt(3);
+                    System.out.println(val); //이 줄은 왜 들어가지?
+                    throw new Exception("범위 초과");
+                } ).withMessageMatching("charAt2() - Index out of bounds issue");
     }
 }
