b6518951202fe3944a2644ee701f5fe4bc8198c1
muttjimmypark <muttpark57@gmail.com>
2022-04-18 12:54:02 +0900

    fix(Compare.java)

--- src/main/java/baseball/Compare.java
+++ src/main/java/baseball/Compare.java
@@ -1,10 +1,11 @@
 package baseball;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Compare {
-    List<Integer> computerNumbers;
-    List<Integer> playerNumbers;
+    private List<Integer> computerNumbers;
+    private List<Integer> playerNumbers;
     private int exist = 0;
     private int strike = 0;
 
@@ -16,19 +17,28 @@ public class Compare {
     }
 
     public void existCount() {
-        for (int i = 0; i < 3; i++) {
-            if (computerNumbers.contains(playerNumbers.get(i))) {
-                exist++;
-            }
-        }
+//        for (int i = 0; i < 3; i++) {
+//            if (computerNumbers.contains(playerNumbers.get(i))) {
+//                exist++;
+//            }
+//        }
+
+        exist = (int) playerNumbers.stream()
+                .filter(playerNumber -> computerNumbers.contains(playerNumber))
+                .count();
+
     }
 
     public void strikeCount() {
-        for (int i = 0; i < 3; i++) {
-            if (computerNumbers.get(i).equals(playerNumbers.get(i))) {
-                strike++;
-            }
-        }
+//        for (int i = 0; i < 3; i++) {
+//            if (computerNumbers.get(i).equals(playerNumbers.get(i))) {
+//                strike++;
+//            }
+//        }
+
+        strike = (int) IntStream.range(0,3)
+                .filter(i -> (computerNumbers.get(i).equals(playerNumbers.get(i))))
+                .count();
     }
 
     public int getExist() {
